5e908f208a8ced8a88f3117ae6b197c32d342f58
Gijs Kruize <gbmkruize@gmail.com>
2022-10-03 11:10:05 +0200

    Update solve.py

--- solve.py
+++ solve.py
@@ -22,34 +22,30 @@ def solve_exercise(exercise_location : str, answer_location : str):
     answer_location might not exist yet and, hence, might still need to be created.
     """
     
-    # Open file at exercise_location for reading.
+    # Open file at exercise_location for reading
     with open(exercise_location, "r") as exercise_file:
-        # Deserialize JSON exercise data present in exercise_file to corresponding 
-    Python exercise data 
+        # Deserialize JSON exercise data present in exercise_file to corresponding  Python exercise data 
         exercise = json.load(exercise_file)
         
     ### Parse and solve ###
     # Check type of exercise
     if exercise["type"] == "polynomial_arithmetic":
         # Check what task within the polynomial arithmetic tasks we need to perform
-        if exercise["task"] = "addition":
+        if exercise["task"] == "addition":
             # Solve polynomial arithmetic addition exercise
             pass
-        elif exercise["task"] = "subtraction":
+        elif exercise["task"] == "subtraction":
             # Solve polynomial arithmetic subtraction exercise
             pass
         # et cetera
     else: # exercise["type"] == "finite_field_arithmetic"
-        # Check what task within the finite field arithmetic tasks we need to 
-perform
-        if exercise["task"] = "addition":
+        # Check what task within the finite field arithmetic tasks we need to perform
+        if exercise["task"] == "addition":
             # Solve finite field arithmetic addition exercise
             pass
         # et cetera
-    # Open file at answer_location for writing, creating the file if it does not 
-exist yet
+    # Open file at answer_location for writing, creating the file if it does not exist yet
     # (and overwriting it if it does already exist).
     with open(answer_location, "w") as answer_file:
-        # Serialize Python answer data (stored in answer) to JSON answer data and 
-write it to answer_file
+        # Serialize Python answer data (stored in answer) to JSON answer data and write it to answer_file
         json.dump(answer, answer_file, indent=4)
