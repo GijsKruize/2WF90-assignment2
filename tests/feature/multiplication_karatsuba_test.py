664025233711cfabe110cf2b8535684c128d56da
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-10-03 23:18:49 +0200

    Remove old tests

--- tests/feature/multiplication_karatsuba_test.py
+++
@@ -1,9 +0,0 @@
-from .assert_answer import assert_exercise
-
-
-def test_realistic_exercise():
-    assert_exercise("realistic", 8)
-
-
-def test_simple_exercise():
-    assert_exercise("simple", 3)
