dbb6c9da5618a5dbdb581e15d096c3646a6b5182
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-10-05 10:14:29 +0200

    Add multiplication

--- polynomial_arithmetic/multiply.py
+++ polynomial_arithmetic/multiply.py
@@ -1,2 +1,16 @@
-def multiplication():
-    pass
+from helpers import equalize_array_length, remove_leading_zeros
+
+
+def multiplication(modulus: int, f: list[int], g: list[int]):
+    f, g, max_length = equalize_array_length(f, g)
+
+    # TODO rewrite
+    result_length = (2 * len(f)) - 1
+    result = [0] * result_length
+
+    for i in range(max_length):
+        for j in range(max_length):
+            result[i + j] += f[i] * g[j]
+            result[i + j] = result[i + j] % modulus
+
+    return remove_leading_zeros(result)
