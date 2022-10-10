3435d320f9c0577ee51947c826e1c1fac47f8378
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-10-10 18:11:39 +0200

    Add assert to long_div unit test

--- tests/unit/long_division_test.py
+++ tests/unit/long_division_test.py
@@ -4,4 +4,4 @@ from polynomial_arithmetic.long_division import long_division
 def test_lecture_example():
     f = [6, 1, 10]
     g = [1, 2]
-    long_division(99, f, g)
+    assert [97, 5], [8] == long_division(99, f, g)
