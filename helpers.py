24665be93d791cb56dea3fb1c151401dd28ee35c
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-10-10 17:57:32 +0200

    Fix if array is only zeros and change naming scheme a bit

--- helpers.py
+++ helpers.py
@@ -17,7 +17,7 @@ def equalize_array_length(f: list[int], g: list[int]):
 
 def remove_leading_zeros(array: list[int]):
     for i in range(len(array) - 1, -1, -1):
-        if array[i] != 0:
+        if array[i] != 0 or i == 0:
             return array[:i + 1]
 
     if not array:
@@ -33,7 +33,7 @@ def remove_duplicates(array: list[T]):
     return list(dict.fromkeys(array))
 
 
-def degree(f: list[int]):
+def get_degree(f: list[int]):
     f = remove_leading_zeros(f)
 
     return len(f) - 1
@@ -43,5 +43,5 @@ def reduction(modulus: int, f: list[int]):
     return [x % modulus for x in f]
 
 
-def leading_coefficent(f: list[int]):
+def get_leading_coefficent(f: list[int]):
     return remove_leading_zeros(f)[-1]
