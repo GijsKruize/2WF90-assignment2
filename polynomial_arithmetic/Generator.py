07c3970002b0b0686b15795e146c2c6815f5ec2d
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-10-10 18:03:29 +0200

    Cleanup other code

--- polynomial_arithmetic/Generator.py
+++ polynomial_arithmetic/Generator.py
@@ -1,20 +1,22 @@
 import random
-from irredCheck import irreducibleTest
+from irreducible_check import irreducible_test
 
 def findIrred(n, p):
     f = [random.randint(1, p-1)]
-    i=0
-    while i<n-1:
+    i = 0
+
+    while i < n - 1:
         f.append(random.randint(0, p - 1))
 
-    irreducible=False
+    irreducible = False
+
     while not irreducible:
-        if irreducibleTest(f,p):
+        if irreducible_test(f, p):
            irreducible = True
         else:
             f = [random.randint(1, p - 1)]
             i = 0
-            while i < n-1:
+            while i < n - 1:
                 f.append(random.randint(0, p - 1))
 
     return f
