51ea130901a53a90d865091fd1aceb9ee2b1e4d7
Gijs Kruize <gbmkruize@gmail.com>
2022-10-27 22:59:35 +0200

    updates

--- tests/feature/primitve_check_test copy.py
+++ tests/feature/primitve_check_test copy.py
@@ -10,10 +10,3 @@ def test_simple_exercise_1():
 
 def test_simple_exercise_2():
     assert_exercise("simple", 18)
-
-def test_simple_exercise_3():
-    assert_exercise("simple", 17)
-
-
-def test_simple_exercise_4():
-    assert_exercise("simple", 19)
