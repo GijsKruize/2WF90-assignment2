b5ee4ad34e32be481894317a981598b5fffa94ed
JordyVerhoeven <33899722+JordyVerhoeven@users.noreply.github.com>
2022-10-27 23:01:32 +0200

    small patch

--- tests/feature/euclid_test.py
+++ tests/feature/euclid_test.py
@@ -2,11 +2,11 @@ from tests.feature.assert_answer import assert_exercise
 
 
 def test_realistic_exercise_1():
-    assert_exercise("realistic", 5)
+    assert_exercise("realistic", 11)
 
 
 def test_realistic_exercise_2():
-    assert_exercise("realistic", 17)
+    assert_exercise("realistic", 16)
 
 
 def test_simple_exercise_1():
